80abb1b08f2b39141ee0534bef3aa110d57260da
yukaisep22 <43335099+yukaisep22@users.noreply.github.com>
2018-10-15 22:28:49 +0800

    Create time convert2.0.cpp

---
+++ w03/time convert2.0.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int sec;
+    int hh,mm,ss;
+    printf("Enter second:");
+    scanf("%d",&sec);
+    while(sec>=0)
+    {
+        hh=sec/3600;
+        mm=sec%3600/60;
+        ss=sec%60;
+        printf("%d sec = %02d:%02d:%02d\n",sec,hh,mm,ss);
+        return main();
+    }
+}
